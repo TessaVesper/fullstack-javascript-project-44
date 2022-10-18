b9208ed0064f65ce49a5dca7ce2759e714f7711b
Tessa Vesper <alexandra.matsuka@yahoo.com>
2022-10-18 19:49:56 +0300

    feat: add brain-calc game

---
+++ games/brain-even-game.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { gameIteration } from "../src/index.js";
+
+const getRandomValue = () => Math.floor(Math.random() * 101);
+const getCorrectAnswer = (number) => (number % 2 === 0 ? "yes" : "no");
+
+export function brainEven() {
+  const titleEven = `Answer "yes" if the number is even, otherwise answer "no".`;
+
+  const evenGame = () => {
+    const number = getRandomValue();
+    const correctAnswer = getCorrectAnswer(number);
+
+    const getPair = [number, correctAnswer];
+    return getPair;
+  };
+
+  gameIteration(titleEven, evenGame);
+}
