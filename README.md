43fa7e6b474bc1f74aaa40f5cd22db142a0aee16
Tessa Vesper <alexandra.matsuka@yahoo.com>
2022-10-18 21:41:25 +0300

    feat: add progression-game

--- README.md
+++ README.md
@@ -4,14 +4,18 @@
 
 <a href="https://codeclimate.com/github/TessaVesper/fullstack-javascript-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/29f30e7e05f240853266/maintainability" /></a>
 
-#### asciinema recording for Even Game:
+#### asciinema recording of Even Game:
 
 https://asciinema.org/a/IsDhgYCmjlWDinZ6VPTu6iDsl
 
-#### asciinema recording for Calc Game:
+#### asciinema recording of Calc Game:
 
 https://asciinema.org/a/DrH46P2O8yJri7TErlyflST8i
 
-#### asciinema recording for GCD Game:
+#### asciinema recording of GCD Game:
 
 https://asciinema.org/a/qZteKW3WIQA9WOehLilSzp4Qr
+
+#### asciinema recording of Progression Game:
+
+https://asciinema.org/a/e8FIOjpt074YCkRx4YiHh4U7q
---
+++ games/brain-progression-game.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import { gameIteration } from "../src/index.js";
+
+const getRandomValue = () => Math.floor(Math.random() * 101);
+const getRandomValuePr = () => Math.floor(Math.random() * (10 - 5 + 1) + 5);
+const getRandomOperation = (array) =>
+  array[Math.floor(Math.random() * array.length)];
+
+const getArr = (start, stop, step) =>
+  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
+
+export function brainProgression() {
+  const titleProgression = `What number is missing in the progression?`;
+
+  const progressionGame = () => {
+    const start = getRandomValue();
+    const step = getRandomValuePr();
+    const length = getRandomValuePr();
+    const stop = start + step * length;
+
+    let arr = getArr(start, stop, step);
+
+    let index = arr.indexOf(getRandomOperation(arr));
+    let coveredValue = arr[index];
+    arr[index] = "..";
+    arr = arr.join(" ");
+
+    const question = `${arr}`;
+    const correctAnswer = `${coveredValue}`;
+
+    const getPair = [question, correctAnswer];
+    return getPair;
+  };
+
+  gameIteration(titleProgression, progressionGame);
+}
